61df56fd399fa1d6e0f682d19e6756edab7ab973
hamzaMahdi <hamzamahdi96@gmail.com>
2019-10-19 16:52:14 -0400

    steven's code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot_hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot_hardware.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 
 public class robot_hardware {
-    static DcMotor lf_drive,lr_drive,rr_drive,rf_drive,arm;
+    static DcMotor lf_drive,lr_drive,rr_drive,rf_drive,arm,left_intake,right_intake;
     static Servo claw1,claw2,grip;
     static BNO055IMU imu;
     public robot_hardware(HardwareMap hw, Telemetry telemetry){
@@ -18,33 +18,32 @@ public class robot_hardware {
             lr_drive = hw.get(DcMotor.class, "lr_drive");
             rf_drive = hw.get(DcMotor.class, "rf_drive");
             rr_drive = hw.get(DcMotor.class, "rr_drive");
+            left_intake = hw.get(DcMotor.class,"LeftIntake");
+            right_intake = hw.get(DcMotor.class,"RightIntake");
             claw1 = hw.get(Servo.class, "claw1");
             claw2 = hw.get(Servo.class, "claw2");
             arm = hw.get(DcMotor.class, "arm");
             grip = hw.get(Servo.class, "grip");
             imu = hw.get(BNO055IMU.class, "imu");
+            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+            parameters.mode = BNO055IMU.SensorMode.IMU;
+            parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+            parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+            parameters.loggingEnabled = false;
+            imu.initialize(parameters);
+
+            telemetry.addData("Mode", "calibrating...");
+            telemetry.update();
+
+            // make sure the imu gyro is calibrated before continuing.
+            while (!imu.isGyroCalibrated()) {
+                //idle
+            }
         }
         catch (Exception e){
             telemetry.addLine("failed initialization, check your ids");
             telemetry.update();
-        }
-    }
-
-    public void init_imu(Telemetry telemetry){
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-
-        parameters.mode = BNO055IMU.SensorMode.IMU;
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.loggingEnabled = false;
-        imu.initialize(parameters);
-
-        telemetry.addData("Mode", "calibrating...");
-        telemetry.update();
-
-        // make sure the imu gyro is calibrated before continuing.
-        while (!imu.isGyroCalibrated()) {
-            //idle
         }
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sample_class.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sample_class.java
@@ -1,14 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 //this is a sample class that you can use to make teleop or auto programs
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp(name="test")
 public class sample_class extends LinearOpMode {
     @Override
-    public void runOpMode(){
+    public void runOpMode() throws RuntimeException{
         robot_hardware robot = new robot_hardware(hardwareMap,telemetry);
-        robot.init_imu(telemetry);
         action_lib action = new action_lib(robot);
         waitForStart();
-x        //code goes here
+        while(true){
+            double forward = this.gamepad1.left_stick_x;
+            double strafe = this.gamepad1.left_stick_y;
+            double turn = -this.gamepad1.right_stick_x;
+            action.run_drive(forward-strafe-turn,forward+strafe+turn,
+                    -forward+strafe+turn,forward+strafe-turn);
+            boolean intake = this.gamepad1.a;
+            boolean outake = this.gamepad1.b;
+            if (intake == true){
+                robot.left_intake.setPower(1);
+                robot.right_intake.setPower(1);
+            }
+            else if(outake){
+                robot.left_intake.setPower(-1);
+                robot.right_intake.setPower(-1);
+
+            }
+            else{
+                robot.left_intake.setPower(0);
+                robot.right_intake.setPower(0);
+
+            }
+        }
     }
 }
