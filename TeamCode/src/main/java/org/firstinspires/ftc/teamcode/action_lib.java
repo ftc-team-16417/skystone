0b98294fae1888ee4b4ad96091cfe7de76c51e10
dreamliner787 <richardtan787@gmail.com>
2019-10-27 10:11:44 -0400

    Update action_lib.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/action_lib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/action_lib.java
@@ -1,25 +1,29 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class action_lib {
+
     robot_hardware robot;
-    public action_lib(robot_hardware robot){
+    action_lib(robot_hardware robot){
         this.robot = robot;
     }
 
-    public void drive(){
-        this.robot.arm.setTargetPosition(700);
-        this.robot.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-
-        robot_hardware.arm.setPower(1);
-        while(robot.arm.isBusy()){
-            //idle
-        }
-        this.robot.arm.setPower(0);
-        this.robot.arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    //your functions go here
+    void run_drive(double lf,double rf,double rr,double lr){
+        this.robot.lf_drive.setPower(lf);
+        this.robot.lr_drive.setPower(lr);
+        this.robot.rr_drive.setPower(rr);
+        this.robot.rf_drive.setPower(rf);
     }
-
+    void stop_drive(){
+        this.robot.lf_drive.setPower(0);
+        this.robot.lr_drive.setPower(0);
+        this.robot.rr_drive.setPower(0);
+        this.robot.rf_drive.setPower(0);
+    }
+    void pickUp(){
+        this.robot.claw1.setPosition(0);
+        this.robot.arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
 }
