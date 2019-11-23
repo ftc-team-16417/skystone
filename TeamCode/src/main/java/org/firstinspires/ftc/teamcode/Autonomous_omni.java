9d1a792db66e491b83c3dfd61ea6928bb84605cc
dreamliner787 <richardtan787@gmail.com>
2019-11-23 09:57:55 -0500

    Create Autonomous_omni.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_omni.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Autonomous_omni extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode(){
+        robot_hardware robot = new robot_hardware();
+        action_lib action = null;
+        imu_lib imu = null;
+        waitForStart();
+        while(opModeIsActive()) {
+            
+
+        }
+
+    }
+}
