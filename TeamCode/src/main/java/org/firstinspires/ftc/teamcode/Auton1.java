2d9640a6535dd2cf82be1b58313f887e28edd968
hmy20040204 <hmy20040204@gmail.com>
2019-11-30 18:02:57 -0500

    AddedTestingAutonCode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton1.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+//this is a sample class that you can use to make teleop or auto programs
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+@Autonomous(name="testNotAnAuton")
+public class Auton1 extends LinearOpMode {
+    @Override
+    public void runOpMode(){
+        robot_hardware robot = new robot_hardware(hardwareMap,telemetry);
+        action_lib action = new action_lib(robot);
+        imu_lib imu = new imu_lib(robot, action);
+        while(robot.arm.getCurrentPosition()>10) robot.arm.setPower(-1);
+        imu.resetAngle();
+        waitForStart();
+        //code goes here
+
+        imu.goStraightIMU(1, 0,20,0.5,robot);
+        imu.rotate(90,0.5);
+
+    }
+}
